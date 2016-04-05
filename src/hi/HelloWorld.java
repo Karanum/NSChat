4e4583ab2c4b4fae744d12aef805db35884e91f0
Pieter Jan <pieterjaninfo@gmail.com>
2016-04-05 12:47:31 +0200

    Master hacker program added with new package

---
+++ src/hi/HelloWorld.java
@@ -0,0 +1,10 @@
+package hi;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Boom boom boom, now let me hear you say wayooo~~");
+	}
+
+}
