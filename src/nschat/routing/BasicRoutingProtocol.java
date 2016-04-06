ebf80c9b5a0ffee98f27cb05ddd58b9227b6332d
Pieter Jan <pieterjaninfo@gmail.com>
2016-04-06 15:29:00 +0200

    changes to BasicRoutingProtocol.java

--- src/nschat/routing/BasicRoutingProtocol.java
+++ src/nschat/routing/BasicRoutingProtocol.java
@@ -1,8 +1,33 @@
 package nschat.routing;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Distance vector routing protocol
+ * @author Pieter Jan
+ *
+ */
 public class BasicRoutingProtocol {
 
+	private List<byte[]> sendBuffer;
+	private double RTT;
+	
+	
+	
 	public BasicRoutingProtocol() {
-		
+		packetbuffer = new ArrayList<byte[]>();
 	}
+	
+	
+	public void receivePacket(byte[] packet) {
+		byte[] old = sendBuffer.get(TCP.getAck(packet));
+		double sendTime = TCP.getTimestamp(old);
+		RTT = System.currentTimeMillis() - sendTime;
+	}
+	
+	
+	
+	
+	
 }
