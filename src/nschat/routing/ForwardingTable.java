b324611896c7a0594de46d5b78ef08be31bde1c4
Pieter Jan <pieterjaninfo@gmail.com>
2016-04-07 09:07:32 +0200

    BasicRoute.java added to routing package

--- src/nschat/routing/ForwardingTable.java
+++ src/nschat/routing/ForwardingTable.java
@@ -1,14 +1,17 @@
-package nschat.tcp;
+package nschat.routing;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ForwardingTable {
 	
 	// FORWARDING TABLE CONTAINS: DESTINATION, COST, VIA NEIGHBOUR!!!
 	private HashMap<Byte, Long> forwardingTable;
+	private HashMap<Byte, Map<Long, Byte>> fowardingTable2;
 	
 	public ForwardingTable() {
 		forwardingTable = new HashMap<Byte, Long>();		//TODO extend forwarding-table
+		
 	}
 	
 	public void addRoute(byte sender, long cost) {
