7c7b408e4755cdc4a2f60d62a8238da2eb2626e7
Bart <bart@LaptopBdeb>
2016-04-07 10:13:58 +0200

    Basic GUI made

--- src/nschat/ui/BasicGUI.java
+++ src/nschat/ui/BasicGUI.java
@@ -1,32 +1,39 @@
 package nschat.ui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
-import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import net.miginfocom.swing.MigLayout;
-import java.awt.GridBagLayout;
 import javax.swing.JScrollPane;
-import java.awt.GridBagConstraints;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JScrollBar;
-import javax.swing.JTextPane;
-import org.eclipse.wb.swing.FocusTraversalOnArray;
-import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+/**
+ * Basic GUI that can print any text and accept printed text from the user.
+ * @author Bart Meyers
+ *
+ */
 public class BasicGUI extends JFrame {
+	
 	private JTextField textField;
+	private JTextArea textArea;
+	private JMenuItem menuExit;
+	private JButton sendButton;
 
+	private class Listener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			printText(textField.getText());
+			textField.setText("");
+		}
+	}
+	
 	/**
 	 * Launch the application.
 	 */
@@ -47,6 +54,8 @@ public class BasicGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public BasicGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Chat21");
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -54,21 +63,52 @@ public class BasicGUI extends JFrame {
 		JMenu mnFile = new JMenu("Menu");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmExit = new JMenuItem("Exit");
-		mnFile.add(mntmExit);
+		menuExit = new JMenuItem("Exit");
+		mnFile.add(menuExit);
 		getContentPane().setLayout(new MigLayout("", "[grow][grow][][][][][][][][][][][][][]", "[grow][][][][][][][][][]"));
 		
 		JScrollPane scrollPane = new JScrollPane();
-		getContentPane().add(scrollPane, "cell 1 0 14 9,grow");
+		getContentPane().add(scrollPane, "cell 0 0 15 9,grow");
 		
-		JTextArea textArea = new JTextArea();
+		textArea = new JTextArea();
+		textArea.setEditable(false);
 		scrollPane.setViewportView(textArea);
 		
 		textField = new JTextField();
-		getContentPane().add(textField, "cell 1 9 13 1,grow");
+		getContentPane().add(textField, "cell 0 9 14 1,grow");
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
-		getContentPane().add(btnNewButton, "cell 14 9,grow");
+		sendButton = new JButton("Send");
+		getContentPane().add(sendButton, "cell 14 9,grow");
+		
+		sendButton.addActionListener(new Listener());
+		textField.addActionListener(new Listener());
+
+		menuExit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);									//TODO change to proper exit mechanism	
+			}
+		});
+		
+	}
+	
+	//TODO change such that messages are ordered by sending time.
+	/**
+	 * Print line of text on the textArea.
+	 * @param text
+	 */
+	public void printText(String text) {
+		textArea.append(text + "\n");
+	}
+	
+	/**
+	 * Print a line of text with the name of the sender.
+	 * @param text
+	 * @param name
+	 */
+	public void printText(String text, String name) {
+		textArea.append(text + " -" + name + "\n");
 	}
 }
