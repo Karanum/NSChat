f66397641eb23203f8355460990701f9351a0c03
Karanum <hyougakazu@gmail.com>
2016-04-07 09:24:03 +0200

    Now creates packets with source/destination

--- src/nschat/multicasting/Connection.java
+++ src/nschat/multicasting/Connection.java
@@ -13,7 +13,6 @@ public class Connection implements Runnable {
 			cast = new Multicast(receivingBuffer);
 		} catch (IOException e) {
 			throw e;
-			//UI.error("Port already in use restart your system!");
 		}
 		receivingBuffer = new ReceivingBuffer();
 		sendingBuffer = new SendingBuffer();
