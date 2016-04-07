a69ca701903e1300fc0f77c1befca1dab6d767bf
Pieter Jan <pieterjaninfo@gmail.com>
2016-04-07 08:52:24 +0200

    minor changes to ForwardingTable.java

--- src/nschat/tcp/ForwardingTable.java
+++ src/nschat/tcp/ForwardingTable.java
@@ -1,21 +1,26 @@
 package nschat.tcp;
 
-import java.util.concurrent.ConcurrentHashMap;
-
-import client.BasicRoute;
+import java.util.HashMap;
 
 public class ForwardingTable {
 	
-	
-	
-	private ConcurrentHashMap<Integer, BasicRoute> forwardingTable;
+	// FORWARDING TABLE CONTAINS: DESTINATION, COST, VIA NEIGHBOUR!!!
+	private HashMap<Byte, Long> forwardingTable;
 	
 	public ForwardingTable() {
-		
+		forwardingTable = new HashMap<Byte, Long>();		//TODO extend forwarding-table
 	}
 	
 	public void addRoute(byte sender, long cost) {
+		// Check if the route already exists in the forwarding table add if not, else compare cost.
 		
+		if (!forwardingTable.containsKey(sender)) {
+			forwardingTable.put(sender, cost);
+		} else {
+			if (cost < forwardingTable.get(sender)) {
+				forwardingTable.put(sender, cost);
+			}
+		}
 	}
 	
 	
