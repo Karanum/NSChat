ea1dea438bd95ce2d3544c3d9618ada32d03751e
Karanum <hyougakazu@gmail.com>
2016-04-08 08:11:24 +0200

    Now able to send and receive messages

--- src/nschat/Program.java
+++ src/nschat/Program.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 import java.io.IOException;
 
 import nschat.multicasting.Connection;
+import nschat.multicasting.ReceivingBuffer;
+import nschat.multicasting.SendingBuffer;
 import nschat.ui.BasicGUI;
 
 /**
@@ -11,17 +13,24 @@ import nschat.ui.BasicGUI;
  * @author Karanum
  */
 public class Program {
-
+	
 	public static void main(String[] args) {
 		new Program();
 	}
 	
 	private Connection conn;
+	private BasicGUI ui;
+	private static boolean running = false;
 	
+	/**
+	 * Starts a new instance of the program, can only be called once.
+	 */
 	public Program() {
+		if (running) { return; }	//Guard in case someone accidentally makes a new Program
+		
 		System.out.println("Setting up connection");
 		try {
-			conn = new Connection();
+			conn = new Connection(this);
 		} catch (IOException e) {
 			System.err.println("Could not set up connection: The port number might be in use");
 			//UI.error("Port already in use, please restart your system!");
@@ -35,17 +44,26 @@ public class Program {
 		/*
 		 * Creating the GUI window
 		 */
-		BasicGUI frame = new BasicGUI(this);
+		ui = new BasicGUI(this);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					frame.setVisible(true);
+					ui.setVisible(true);
 					System.out.println("Window created!");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		
+		/*
+		 * Main loop
+		 */
+		running = true;
+		while (running) {
+			conn.send();		//Check for outgoing messages
+			conn.receive();		//Check for incoming messages
+		}
 		
 		/*
 		 * Thread cleanup
@@ -57,8 +75,27 @@ public class Program {
 		}
 	}
 	
+	/**
+	 * Returns the connection of the program.
+	 * @return A Connection object
+	 */
 	public Connection getConnection() {
 		return conn;
+	}
+	
+	/**
+	 * Returns the user interface of the program.
+	 * @return A BasicGUI object
+	 */
+	public BasicGUI getUI() {
+		return ui;
+	}
+	
+	/**
+	 * Tells the program to stop running.
+	 */
+	public void stop() {
+		running = false;
 	}
 
 }
--- src/nschat/multicasting/Connection.java
+++ src/nschat/multicasting/Connection.java
@@ -1,14 +1,22 @@
 package nschat.multicasting;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+
+import nschat.Program;
+import nschat.exception.PacketFormatException;
+import nschat.tcp.Packet;
+import nschat.tcp.Packet.PacketType;
 
 public class Connection implements Runnable {
 	
 	ReceivingBuffer receivingBuffer; 
 	SendingBuffer sendingBuffer;
 	Multicast cast;
+	Program program;
 	
-	public Connection() throws IOException {
+	public Connection(Program program) throws IOException {
+		this.program = program;
 		try {
 			cast = new Multicast(receivingBuffer);
 		} catch (IOException e) {
@@ -21,6 +29,42 @@ public class Connection implements Runnable {
 	@Override
 	public void run() {
 		cast.receiveDatagram();
+	}
+	
+	public void send() {
+		for (byte[] packet : sendingBuffer.getAllFromBuffer()) {
+			DatagramPacket datagram = cast.makeDgramPacket(packet);
+			cast.sendDatagram(datagram);
+		}
+	}
+	
+	public void receive() {
+		byte[] packet;
+		Packet p;
+		while ((packet = receivingBuffer.getNext()) != null) {
+			try {
+				p = new Packet(packet);
+			} catch (PacketFormatException e) { 
+				continue;
+			}
+			
+			PacketType type = p.getPacketType();
+			switch (type) {
+				case TEXT:
+					program.getUI().printText(p.getDataAsString());
+				case FILE:
+					break;
+				case ROUTING:
+					break;
+				case SECURITY:
+					break;
+				default:
+			}
+		}
+	}
+	
+	public ReceivingBuffer getReceivingBuffer() {
+		return receivingBuffer;
 	}
 	
 	public SendingBuffer getSendingBuffer() {
