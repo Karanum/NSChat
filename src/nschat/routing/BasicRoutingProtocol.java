c46c453efd8d6717be7127dbe398d635e36435ed
Pieter Jan <pieterjaninfo@gmail.com>
2016-04-11 09:35:23 +0200

    Javadoc added in BasicRoutingProtocol.java

--- src/nschat/routing/BasicRoutingProtocol.java
+++ src/nschat/routing/BasicRoutingProtocol.java
@@ -15,7 +15,7 @@ import nschat.tcp.SequenceNumbers;
 import nschat.tcp.Packet.PacketType;
 
 /**
- * Distance vector routing protocol.
+ * Distance-vector routing protocol.
  * @author Pieter Jan
  *
  */
@@ -24,7 +24,7 @@ public class BasicRoutingProtocol {
 	private SendingBuffer sendingBuffer;
 	private ForwardingTable forwardingTable;
 	
-	//map person id -> rtt
+	//MAP SENDER -> RTT
 	private Map<Integer, Integer> senderRTT;
 	
 	
@@ -34,6 +34,11 @@ public class BasicRoutingProtocol {
 		senderRTT = new HashMap<Integer, Integer>();
 	}
 
+	/**
+	 * Allows to receive a packet, ensued by getting the RTT of the sender, and updating its
+	 * own forwardingTable.
+	 * @param packet The packet with the routing data.
+	 */
 	public void receivePacket(Packet packet) {
 		int rtt = getRTT(packet);
 		setSenderRTT(rtt, packet.getSender());
@@ -43,6 +48,11 @@ public class BasicRoutingProtocol {
 		forwardingTable.updateTable(routes, packet.getSender());
 	}
 	
+	/**
+	 * Calculates and returns the RTT value of the sender from the given packet.
+	 * @param packet The sender's packet
+	 * @return The RTT value of the sender
+	 */
 	public int getRTT(Packet packet) {
 		byte[] old = sendingBuffer.get(PacketType.ROUTING, packet.getAckNumber());
 		Packet oldPacket;
@@ -56,6 +66,9 @@ public class BasicRoutingProtocol {
 		return rtt;
 	}
 	
+	/**
+	 * Converts this forwardingTable into a Routing packet and add it to the sendingBuffer.
+	 */
 	public void sendPacket() {
 		short seq = SequenceNumbers.get(PacketType.ROUTING);
 		
@@ -103,6 +116,9 @@ public class BasicRoutingProtocol {
 		return bytesList;
 	}
 	
+	/*
+	 * Sets the given RTT value for the given sender in the senderRTT map.
+	 */
 	private void setSenderRTT(int rtt, int sender) {
 		if (senderRTT.containsKey(sender)) {
 			int currentRTT = senderRTT.get(sender);
@@ -112,6 +128,9 @@ public class BasicRoutingProtocol {
 		}
 	}
 	
+	/**
+	 * Returns a map containing all senders with their corresponding RTT cost.
+	 */
 	public Map<Integer, Integer> getSenderRTT() {
 		return senderRTT;
 	}
