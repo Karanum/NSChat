dc8a2a4065a14bb66c05e92c046d43fa3a56a9eb
Karanum <hyougakazu@gmail.com>
2016-04-14 15:45:53 +0200

    Fix commit for untracked files

---
+++ src/nschat/tcp/AckList.java
@@ -0,0 +1,59 @@
+package nschat.tcp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import nschat.multicasting.Connection;
+import nschat.tcp.Packet.PacketType;
+
+/**
+ * List of expected ACKs for each sent packet.
+ * @author Karanum
+ */
+public class AckList {
+
+	private static Map<PacketType, Map<Short, AckList>> instances
+							= new HashMap<PacketType, Map<Short, AckList>>();
+	
+	private short seq;
+	private PacketType type;
+	private List<Integer> remaining;
+	private Connection conn;
+	
+	public AckList(Connection conn, PacketType type, short seq, List<Integer> knownClients) {
+		this.conn = conn;
+		seq = this.seq;
+		type = this.type;
+		
+		remaining = new ArrayList<Integer>();
+		remaining.addAll(knownClients);
+		
+		if (!instances.containsKey(type)) {
+			instances.put(type, new HashMap<Short, AckList>());
+		}
+		instances.get(type).put(seq, this);
+		
+		conn.getTimeout().addPacket(type, seq);
+	}
+	
+	/**
+	 * Marks the packet as having been acknowledged by the specified client.
+	 * @param client The client that acknowledged the packet
+	 */
+	public void setAcknowledged(int client) {
+		remaining.remove(client);
+		if (remaining.isEmpty()) {
+			conn.getTimeout().removePacket(type, seq);
+		}
+	}
+	
+	public static AckList getInstance(PacketType type, short seq) {
+		if (!instances.containsKey(type)) {
+			return null;
+		}
+		return instances.get(type).get(seq);
+	}
+	
+}
---
+++ src/nschat/tcp/Timeout.java
@@ -0,0 +1,92 @@
+package nschat.tcp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nschat.Program;
+import nschat.multicasting.Connection;
+import nschat.multicasting.SendingBuffer;
+import nschat.tcp.Packet.PacketType;
+
+/**
+ * Helper class for packet acknowledgement timeouts. Accessed fully by the AckList class.
+ * @author Karanum
+ */
+public class Timeout {
+
+	private static final byte MAX_RETRANSMISSIONS = 3;
+	private static final long TIMEOUT_INTERVAL = 1000L;
+	
+	private long lastTime;
+	private Connection conn;
+	
+	private Map<PacketType, Map<Short, Long>> lastSent;
+	private Map<PacketType, Map<Short, Byte>> numRetransmissions;
+	
+	public Timeout(Connection conn) {
+		lastTime = System.currentTimeMillis();
+		this.conn = conn;
+		
+		lastSent = new HashMap<PacketType, Map<Short, Long>>();
+		numRetransmissions = new HashMap<PacketType, Map<Short, Byte>>();
+	}
+	
+	public void update() {
+		long deltaTime = System.currentTimeMillis() - lastTime;
+		lastTime = System.currentTimeMillis();
+		
+		for (PacketType type : lastSent.keySet()) {
+			for (Short packet : lastSent.get(type).keySet()) {
+				synchronized (this) {
+					if (!lastSent.containsKey(packet)) {
+						continue;
+					}
+					
+					long lastSentTime = lastSent.get(type).get(packet);
+					long totalTime = lastSentTime + deltaTime;
+					while (totalTime >= TIMEOUT_INTERVAL && numRetransmissions.get(type).get(packet) < MAX_RETRANSMISSIONS) {
+						SendingBuffer buffer = conn.getSendingBuffer();
+						byte[] bytes = buffer.get(type, packet);
+						if (bytes != null) {
+							buffer.add(bytes);
+						}
+	
+						byte num = numRetransmissions.get(type).get(packet);
+						numRetransmissions.get(type).put(packet, (byte) (num + 1));
+						
+						lastSent.get(type).put(packet, System.currentTimeMillis() - totalTime);
+						totalTime -= TIMEOUT_INTERVAL;
+					}
+				}
+				
+				if (numRetransmissions.get(type).get(packet) >= MAX_RETRANSMISSIONS) {
+					removePacket(type, packet);
+				}
+			}
+		}
+	}
+	
+	public void addPacket(PacketType type, short seq) {
+		if (lastSent.containsKey(seq)) {
+			return;
+		}
+		
+		synchronized (this) {
+			if (!lastSent.containsKey(type)) {
+				lastSent.put(type, new HashMap<Short, Long>());
+				numRetransmissions.put(type, new HashMap<Short, Byte>());
+			}
+			
+			lastSent.get(type).put(seq, System.currentTimeMillis());
+			numRetransmissions.get(type).put(seq, (byte) 0);
+		}
+	}
+	
+	public void removePacket(PacketType type, short seq) {
+		synchronized (this) {
+			lastSent.get(type).remove(seq);
+			numRetransmissions.get(type).remove(seq);
+		}
+	}
+	
+}
